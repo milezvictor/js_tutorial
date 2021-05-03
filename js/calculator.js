e71ac6e42e42334df6e2ea9ea4f379437c4218d3
Destiny Brotobor <destinybravos@gmail.com>
2021-05-03 18:08:25 +0100

    Added functions on the calculator

--- js/calculator.js
+++ js/calculator.js
@@ -2,6 +2,9 @@ let numbers = document.querySelectorAll('.number');
 // let numbers = document.getElementsByClassName('number'); //Cannot use the forEach Loop
 let operators = document.querySelectorAll('.operator');
 let screen = document.getElementById('output-value');
+let history = document.getElementById('history-value');
+let history_log = document.getElementById('history-new');
+let currentOperator = '';
 
 // Add Event Listener to all number buttons (Event: click)
 numbers.forEach(function(button) {
@@ -35,16 +38,33 @@ function handleOperation(operator) {
         case 'backspace':
             backspace();
             break;
+
+        case 'sin':
+            findSinOfNumber();
+            break;
+           
+        case 'sqrt':
+            findSquareRoot();
+            break;
+            
+        case 'fact':
+            findFactorial();
+            break;
+        
+        case '=':
+            finalResult(operator);
+            break;
     
         default:
-            alert("We don't know this operator");
+            ArithmeticOperation(operator)
             break;
     }
 }
 
 // A function to clear the calculator screen
 function clearScreen() {
-    screen.innerText = ''
+    screen.innerText = '';
+    history.innerText = '';
 }
 
 // A function to delete one value from the end of the calculator screen
@@ -56,6 +76,84 @@ function backspace() {
         newnumber += numberOnScreen[i];
     }
     screen.innerText = newnumber;
+}
+
+// A function to add the values
+function ArithmeticOperation(operator) {
+    if (history.innerText != '' && screen.innerText != '') {
+        switch (currentOperator) {
+            case '+':
+                history.innerText = parseInt(history.innerText) + parseInt(screen.innerText);
+                break;
+    
+            case '-':
+                history.innerText = parseInt(history.innerText) - parseInt(screen.innerText);
+                break;
+    
+            case '*':
+                history.innerText = parseInt(history.innerText) * parseInt(screen.innerText);
+                break;
+                
+            case '/':
+                history.innerText = parseInt(history.innerText) / parseInt(screen.innerText);
+                break;
+
+            case '%':
+                history.innerText = parseInt(history.innerText) % parseInt(screen.innerText);
+                break;
+
+            case 'comb':
+                Combination();
+                break;
+        }
+    } else {
+        if(screen.innerText != ''){
+            history.innerText = parseInt(screen.innerText);
+        }
+    }
+    logHistory();
+    
+    if (operator == '=') {
+        currentOperator = '';
+    } else {
+        currentOperator = operator;
+        screen.innerText = '';
+    }
+}
+
+function finalResult(operator) {
+    ArithmeticOperation(operator)
+    if (history.innerText != '' && screen.innerText != '') {
+        screen.innerText = history.innerText;
+        history.innerText = '';
+    }
+}
+
+function logHistory() {
+    
+}
+
+function findSinOfNumber(){
+    let number = parseInt(screen.innerText);
+    screen.innerText = Math.sin(number)
+}
+
+function findSquareRoot(){
+    let number = parseInt(screen.innerText);
+    screen.innerText = Math.sqrt(number)
+}
+
+function findFactorial(){
+    let fact = 1;
+    let number = parseInt(screen.innerText);
+    for (let i = 1; i <= number; i++) {
+         fact *= i
+    }
+    screen.innerText = fact;
+}
+
+function Combination(){
+    
 }
 
 document.addEventListener('keyup', function (e) {
